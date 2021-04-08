7c5429e648e98b5f1893a222416572335896d38d
sandakelum00 <randunusandakelum@gmail.com>
2021-04-08 23:38:30 +0530

    Adding html and java file for customer crud operations

---
+++ src/main/java/com/example/project1/model/Customer.java
@@ -0,0 +1,18 @@
+package com.example.project1.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Customer {
+	private String fname;
+	private String lname;
+	private String email;
+	private String password;
+	private int phone;
+	private String address1;
+	private String address2;
+	private String city;
+	private String gender;
+	private Date dob;
+
+}
