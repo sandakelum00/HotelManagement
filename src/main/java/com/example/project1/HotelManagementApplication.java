c95c10d750a64be13005ec5ac3257aa26f08c6a6
sandakelum00 <randunusandakelum@gmail.com>
2021-04-19 13:16:42 +0530

    restore default settings

--- src/main/java/com/example/project1/HotelManagementApplication.java
+++ src/main/java/com/example/project1/HotelManagementApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HotelManagementApplication {
-
+	
 	public static void main(String[] args) {
-		SpringApplication.run(HotelManagementApplication.class, args);
+		SpringApplication.run(HotelManagementApplication.class , args);
 	}
-
+	
 }
